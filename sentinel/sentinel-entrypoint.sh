e24f5e41c02dce13a83a7ecdc2e25ecb4bedab79
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-29 08:13:12 +0700

    feat: add redis sentinel node and volume data

--- sentinel/sentinel-entrypoint.sh
+++ sentinel/sentinel-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# wait for DNS resolution
+sleep 5
+
 sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /etc/redis/sentinel.conf
+sed -i "s/\$REDIS_MASTER_PASSWORD/$REDIS_MASTER_PASSWORD/g" /etc/redis/sentinel.conf
 
 exec docker-entrypoint.sh redis-server /etc/redis/sentinel.conf --sentinel
